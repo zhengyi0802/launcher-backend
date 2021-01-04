9eefeb49191a3dd6326fa4d87b9ef0f467c6ad8b
Chevy Lin <chevylin0802@gmail.com>
2021-01-04 12:53:41 +0800

    update

--- database/seeders/ProductCatagorySeeder.php
+++ database/seeders/ProductCatagorySeeder.php
@@ -14,6 +14,15 @@ class ProductCatagorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $productCatagory = new ProductCatagory;
+        $productCatagory->name = '機上盒';
+        $productCatagory->detail = '機上盒';
+        $productCatagory->save();
+        $productCatagory->name = '聲霸';
+        $productCatagory->detail = '聲霸';
+        $productCatagory->save();
+        $productCatagory->name = '配件';
+        $productCatagory->detail = '配件';
+        $productCatagory->save();
     }
 }
